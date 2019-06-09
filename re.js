8708f5bc9b1489b078e8be847600d76735f8ec1a
unknown <loraltumblr@gmail.com>
2019-06-09 08:36:30 +0700

    9 June 8:36

--- re.js
+++ re.js
@@ -1,27 +1,25 @@
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      } 
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
-  }
+
+function add1(){
+  var inp=document.getElementById("Name").value;
+  test=test+2;
+};
+function add2(){
+  var inp1=document.getElementById("Comment").value;
+  test=test+1;
+};
+  
+function confirm(){
+  if(test==3){
+  alert("info recieved");
+  var block=document.getElementById("block");
+  block.style.display="none";
+  test=0;
+  var obj={
+    name:inp,
+    comment:inp1,
+  };
+  arr.push(obj);
+}else{
+  alert("info not recieved");
 }
+};
